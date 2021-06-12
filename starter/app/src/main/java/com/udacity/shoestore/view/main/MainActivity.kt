cfeafae5a2a5f70c9497279fdb35abefa5c8528a
Lucas Puorto <lucas.puorto@br.experian.com>
2021-06-12 11:59:59 -0300

    add AppBarConfiguration on main activity

---
+++ starter/app/src/main/java/com/udacity/shoestore/view/main/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.udacity.shoestore.view.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.ActivityMainBinding
+import timber.log.Timber
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val navController by lazy { findNavController(R.id.nav_host_fragment) }
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this@MainActivity, R.layout.activity_main)
+        Timber.plant(Timber.DebugTree())
+        setSupportActionBar(binding.toolbar)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.loginFragment)
+        )
+        NavigationUI.setupActionBarWithNavController(this@MainActivity, navController, appBarConfiguration)
+        NavigationUI.setupWithNavController(binding.toolbar, navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp()
+    }
+}
