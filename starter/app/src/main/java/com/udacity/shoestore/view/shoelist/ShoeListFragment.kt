3a36435aefa1385bd1917fadc2b9c34fcc1d7b07
Lucas Puorto <lucas.puorto@br.experian.com>
2021-06-15 16:00:12 -0300

    add item on shoes list

--- starter/app/src/main/java/com/udacity/shoestore/view/shoelist/ShoeListFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/view/shoelist/ShoeListFragment.kt
@@ -14,6 +14,8 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentShoeListBinding
+import com.udacity.shoestore.databinding.ItemShoeBinding
+import com.udacity.shoestore.models.Shoe
 import com.udacity.shoestore.viewmodel.MainViewModel
 
 class ShoeListFragment : Fragment() {
@@ -38,7 +40,7 @@ class ShoeListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        observables()
+        observables()
         binding.fabAddShoe.setOnClickListener { directionToFragmentShoeDetails() }
     }
 
@@ -46,50 +48,22 @@ class ShoeListFragment : Fragment() {
         findNavController().navigate(ShoeListFragmentDirections.actionShoeListFragmentToShoeDetailsFragment())
     }
 
-//    private fun observables() {
-//        shoesListViewModel.shoeListLiveData.observe(viewLifecycleOwner, {
-//            handlerShoeList(it)
-//        })
-//    }
-//
-//    private fun handlerShoeList(shoeList: MutableList<Shoe>) {
-//        shoeList.forEach {
-//            val inflatedViewDataBinding = DataBindingUtil.inflate<ItemShoeBinding>(layoutInflater, R.layout.item_shoe, binding.llShoeList, false)
-//            inflatedViewDataBinding.apply {
-//                tvShoeName.text = getString(R.string.shoe_name_arg, it.name)
-//                tvShoeCompany.text = getString(R.string.company_arg, it.company)
-//                tvShoeSize.text = getString(R.string.shoe_size_arg, it.size.toString())
-//                tvShoeDescription.text = getString(R.string.description_arg, it.description)
-//            }
-//            binding.llShoeList.addView(inflatedViewDataBinding.root)
-//        }
-//    }
+    private fun observables() {
+        viewModel.shoeListLiveData.observe(viewLifecycleOwner, {
+            handlerShoeList(it)
+        })
+    }
 
-
-//    private lateinit var binding: FragmentShoeListBinding
-//    private val viewModel by activityViewModels<MainViewModel>()
-//
-//    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-//        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_shoe_list, container, false)
-//        setHasOptionsMenu(true)
-//        return binding.root
-//    }
-//
-//    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-//        super.onCreateOptionsMenu(menu, inflater)
-//        inflater.inflate(R.menu.menu_frag_shoe_list, menu)
-//    }
-//
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        return NavigationUI.onNavDestinationSelected(item, findNavController()) || super.onOptionsItemSelected(item)
-//    }
-//
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//        binding.fabAddShoe.setOnClickListener { directionToFragmentShoeDetails() }
-//    }
-//
-//    private fun directionToFragmentShoeDetails() {
-//        findNavController().navigate(ShoeListFragmentDirections.actionShoeListFragmentToShoeDetailsFragment())
-//    }
+    private fun handlerShoeList(shoeList: MutableList<Shoe>) {
+        shoeList.forEach { shoe ->
+            val inflatedViewDataBinding = DataBindingUtil.inflate<ItemShoeBinding>(layoutInflater, R.layout.item_shoe, binding.llShoesList, false)
+            inflatedViewDataBinding.apply {
+                tvShoeName.text = getString(R.string.shoe_list_shoe_name, shoe.name)
+                tvShoeCompany.text = getString(R.string.shoe_list_company, shoe.company)
+                tvShoeSize.text = getString(R.string.shoe_list_shoe_size, shoe.size.toString())
+                tvShoeDescription.text = getString(R.string.shoe_list_description, shoe.description)
+            }
+            binding.llShoesList.addView(inflatedViewDataBinding.root)
+        }
+    }
 }
